76cd779d2795142e9b1ce4bbab8e421297948497
majdi <majdifkih123@gmail.com>
2024-10-17 13:58:26 +0200

    Delete & update meeting

--- meetings/urls.py
+++ meetings/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import detail,meetings_list_view
+from .views import add_meeting, detail,meetings_list_view,del_meeting,update_meeting
 
 urlpatterns = [
     path('', meetings_list_view, name='meetings_list_view'),  # List of meetings and rooms
 
     path('detail/<int:id>/', detail, name='detail'),  # Meeting detail
-   
+    
+    path('newMeet', add_meeting, name='newMeet'), 
+    path('delete/', del_meeting, name='del_meeting'),
+    path('update/<int:id>/', update_meeting, name='update_meeting'),
 
 ]
--- meetings/views.py
+++ meetings/views.py
@@ -1,3 +1,4 @@
+from .forms import MeetingForm
 from .models import Room,Meeting  
 from django.shortcuts import render,get_object_or_404, redirect
 from django.http import HttpResponse
@@ -6,7 +7,7 @@ def meetings_list_view(request):
 
     meetings = Meeting.objects.all()  # Get all meetings
 
-    return render(request, 'meetings.html', {'meetings': meetings, })
+    return render(request, 'meetings/meetings.html', {'meetings': meetings, })
 
  
 
@@ -16,4 +17,39 @@ def detail(request, id):
 
     meeting = get_object_or_404(Meeting, id=id)  # Correct model name and function
 
-    return render(request, "details.html", {"meeting": meeting})
+    return render(request, "meetings/details.html", {"meeting": meeting})
+
+def add_meeting(request):
+    if request.method == "POST":
+        form = MeetingForm(request.POST)
+        if form.is_valid():
+            form.save()  # Sauvegarde le meeting si le formulaire est valide
+            return redirect('meetings_list_view')  # Redirige vers une liste de meetings ou une autre page après ajout
+    else:
+        form = MeetingForm()
+
+    return render(request, 'meetings/new.html', {'form': form})
+
+def del_meeting(request):
+    if request.method == 'POST':
+        meeting_id = request.POST.get('meeting_id')  
+        meeting = Meeting.objects.get(pk=meeting_id) 
+        meeting.delete()  
+        return redirect('meetings_list_view')
+    else:
+        return HttpResponse('Error')
+
+
+
+def update_meeting(request, id):
+    meeting = get_object_or_404(Meeting, id=id) 
+    
+    if request.method == "POST":
+        form = MeetingForm(request.POST, instance=meeting)
+        if form.is_valid():
+            form.save() 
+            return redirect('meetings_list_view')
+    else:
+        form = MeetingForm(instance=meeting)
+
+    return render(request, 'meetings/update.html', {'form': form, 'meeting': meeting})
